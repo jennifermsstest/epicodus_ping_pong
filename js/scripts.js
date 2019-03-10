fb8bdabcce1e2e558bf1af0785eec36e9e4ae911
Jennifer Marks <jmarks@squarespace.com>
2019-03-09 17:26:12 -0800

    Add other js options

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,41 @@ var listNumber = function(number) {
   for (var i = 1; i <= number; i++) {
     if (i % 15 === 0) {
       //numbers.push('ping');
-      $(".array").append("<li>" + 'pingpong');
+      $("#array").append("<li>" + 'pingpong');
+      //$(".array").innerHTML("<li>" + 'pingpong');
+      //return "<li>" + 'pingpong';
     } else if (i % 3 === 0) {
-      $(".array").append("<li>" + 'ping');
+      //$(".array").print("<li>" + 'ping');
+      $("#array").append("<li>" + 'ping');
       //numbers.push('pong');
+      //return "<li>" + 'ping';
     } else if (i % 5 === 0) {
-      $(".array").append("<li>" + 'pong');
+      //$(".array").print("<li>" + 'pong');
+      $("#array").append("<li>" + 'pong');
+      //return "<li>" + 'pong';
     //  numbers.push('pingpong');
     } else {
-      $(".array").append("<li>" + (i));
+      //$(".array").print("<li>" + (i));
+      $("#array").append("<li>" + (i));
       //numbers.push(i);
+      //return "<li>" + (i);
       }
-  }
+  } //$("#array")[0].reload();
   };
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#ping").submit(function(event) {
+    event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = listNumber(number)
-    textfield1 = result
 
 $("#result").show();
 $("#ping")[0].reset();
-onSubmit="window.location.reload();"
+$(".array").remove();
+//onSubmit="window.location.reload();"
 //$(".reset")[0].reset();
-  event.preventDefault();
 
-});
+//window.location.reload();
+})
 });
